71048715462bc147d3dc855192c46387b81d544f
parkttule <parkttule0305@gmail.com>
2023-08-19 10:45:21 +0900

    4rd

--- front/walkmeal/auction.js
+++ front/walkmeal/auction.js
@@ -1,54 +1,73 @@
-import {View, Text, TextInput, Button} from 'react-native';
-import React, {useState} from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 import Datalist from './datalist';
 import { styles } from './ordercss';
 
 const Auction = () => {
 
-    
- const initdata = [{
-    id:"1",
-    name:"치킨",
-    sale_price:10,
-    org_price:20000
-  },{
-    id:"2",
-    name:"햄버거",
-    sale_price:10,
-    org_price:7500
-  }];
-  
+  const initdata = [
+    {
+      id: "1",
+      name: "치킨",
+      sale_price: 10,
+      org_price: 20000,
+      coin: 0
+    },
+    {
+      id: "2",
+      name: "햄버거",
+      sale_price: 10,
+      org_price: 7500,
+      coin: 0
+    }
+  ];
+
   const [data, setData] = useState(initdata);
 
-  const [coin, setCoin] = useState(0);
+  const valuecheck = (coin, price, itemId) => {
+    if (coin > price) {
+      const updatedData = data.map(item => {
+        if (item.id === itemId) {
+          return { ...item, sale_price: coin };
+        }
+        return item;
+      });
 
-  const valuecheck = (itemID) => {
-    const item = data.find(item => item.id === itemID);
-
-    if(coin > item.sale_price) {
-        setData(prevdata => ({...prevdata, sale_price: coin}));
-        alert('배팅하였습니다.');
-  } else {
-    alert('경매 가격보다 낮게 적을 수 없어요');
-    setCoin(0);
+      setData(updatedData);
+      alert('경매금액을 성공적으로 입력하였습니다!');
+    } else {
+      alert('경매 최소 금액보다 높게 작성해주세요');
+    }
   }
+
+  const handleCoinChange = (itemId, coinValue) => {
+    const updatedData = data.map(item => {
+      if (item.id === itemId) {
+        return { ...item, coin: coinValue };
+      }
+      return item;
+    });
+
+    setData(updatedData);
+  };
 
   return (
     <View style={styles.container}>
-        {data.map(item => (
-            <View style={styles.box}>
-                <Text key={item.id} style={styles.name}>{item.name} 경매 시작!</Text>
-                <Text>{item.sale_price}/start - 현재 나의 배팅가</Text>
-                <TextInput 
-                    placeholder='금액을 입력하세요'
-                    value={coin}
-                    onChangeText={text => setCoin(text)}
-                />
-                {/* <Button title="Buy" onPress={valuecheck}/> */}
-            </View>
-        ))}
+      {data.map(item => (
+        <View style={styles.box} key={item.id}>
+          <Text style={styles.name}>{item.name} 경매 시작!</Text>
+          <Text>현재 {item.sale_price}원 - 현재 나의 배팅가</Text>
+          <TextInput
+            placeholder='금액을 입력하세요'
+            value={item.coin.toString()}
+            onChangeText={text => handleCoinChange(item.id, parseInt(text, 10))}
+            keyboardType="numeric"
+          />
+          <Button title="Buy" onPress={() => valuecheck(item.coin, item.sale_price, item.id)} />
+        </View>
+      ))}
     </View>
   );
 }
-}
+
 export default Auction;
